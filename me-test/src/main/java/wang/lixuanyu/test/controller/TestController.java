00a4552a217056def79a8c11a3e4f0d8353a2019
1562968743@qq.com <Aa980426>
2021-11-28 15:46:57 +0800

    .

--- me-test/src/main/java/wang/lixuanyu/test/controller/TestController.java
+++ me-test/src/main/java/wang/lixuanyu/test/controller/TestController.java
@@ -1,6 +1,15 @@
 package wang.lixuanyu.test.controller;
 
-@RequestMapping
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
 public class TestController {
 
+	@RequestMapping("/info")
+	public String info(String name){
+		System.out.println(name);
+		return "成功！";
+	}
 }
